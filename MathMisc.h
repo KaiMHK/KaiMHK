a36a37c3b116186a0a6cb4ddd04432f82f708557
KaiMHK <106705565+KaiMHK@users.noreply.github.com>
2022-06-01 13:46:12 -0700

    Update MathMisc.h

--- MathMisc.h
+++ MathMisc.h
@@ -1,22 +1,4 @@
-/*
- *
- * MicsMath.h, release 3.1.  December 20, 2006.
- *
- * Author: Samuel R. Buss
- *
- * Software accompanying the book
- *		3D Computer Graphics: A Mathematical Introduction with OpenGL,
- *		by S. Buss, Cambridge University Press, 2003.
- *
- * Software is "as-is" and carries no warranty.  It may be used without
- *   restriction, but if you modify it, please change the filenames to
- *   prevent confusion between different versions.  Please acknowledge
- *   all use of the software in any publications or products based on it.
- *
- * Bug reports: Sam Buss, sbuss@ucsd.edu.
- * Web page: http://math.ucsd.edu/~sbuss/MathCG
- *
- */
+
 
 #ifndef MATH_MISC_H
 #define MATH_MISC_H
